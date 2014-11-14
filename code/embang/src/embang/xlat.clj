e31c78043e6bf959a3f0fe8c9b630dabef6a949a
David Tolpin <david.tolpin@gmail.com>
2014-11-14 14:51:05 +0000

    some tests

--- code/embang/src/embang/xlat.clj
+++ code/embang/src/embang/xlat.clj
@@ -11,14 +11,14 @@
       (let [[expr & exprs] exprs]
         (if (and (seq? expr) (= (first expr) 'define))
           (let [[name value] (rest expr)]
-            `((let [~name ~(expression value)]
+            `((~'let [~name ~(expression value)]
                 ~@(elist exprs))))
           (cons (expression expr) (elist exprs)))))))
 
 (defn alambda
   "translates lambda to fn"
   [[parms & body]]
-  `(fn
+  `(~'fn
      ~(if (list? parms)
         `[~@parms]
         `[& ~parms])
@@ -27,34 +27,24 @@
 (defn alet
   "translates let"
   [[bindings & body]]
-  `(let [~@(mapcat (fn [[name value]]
+  `(~'let [~@(mapcat (fn [[name value]]
                      [name (expression value)])
                    bindings)]
      ~@(elist body)))
 
-(defn aif
-  "translates if to its three-argument form"
-  [[cnd thn els :as args]]
-  (assert (#{2 3} (count args)) (str "illegal if: " `(~'if ~@args)))
-  `(~'if ~(expression cnd) ~(expression thn) ~(expression els)))
-
 (defn acond 
   "translates cond to nested ifs"
   [[& clauses :as args]]
-  (when (seq clauses)
-    (let [[[cnd expr] & clauses] clauses]
-      (if (= cnd 'else)
-        (do
-          (assert (empty? clauses)
-                  (str "else clause must be last: " `(~'cond ... ~@args)))
-          (expression expr))
-        `(~'if ~(expression cnd) ~(expression expr)
-           ~(acond clauses))))))
+  `(~'cond ~@(mapcat (fn [[cnd expr]]
+                       [(if (= cnd 'else) :else
+                          (expression cnd))
+                        (expression expr)])
+                     clauses)))
 
 (defn abegin
   "translates begin to do"
   [[& body]]
-  `(do ~@(elist body)))
+  `(~'do ~@(elist body)))
 
 (defn aform
   "translates compatible forms and function applications"
@@ -70,10 +60,9 @@
           quote  expr
           lambda (alambda args)
           let    (alet args)
-          if     (aif args)
           cond   (acond args)
           begin  (abegin args)
-          ;; other forms (and, or, application) have  compatible structure
+          ;; other forms (if, and, or, application) have  compatible structure
           (aform expr)))
       ()) ; anglican allows unquoted empty list
     expr))
@@ -86,14 +75,14 @@
       (let [[[kwd & args :as d] & ds] ds]
         (case kwd
           assume (let [[name value] args]
-                   `((let [~name ~(expression value)]
+                   `((~'let [~name ~(expression value)]
                        ~@(dlist ds))))
-          observe (cons `(~'observe ~@(map expression args)) (dlist ds))
-          predict (cons `(~'predict ~@(map expression args)) (dlist ds))
+          (observe predict) (cons `(~kwd ~@(map expression args))
+                                  (dlist ds))
           (assert false (str "unrecognized directive: " d)))))))
 
 (defn program
   "translates anglican program into clojure function"
   [& p]
-  `(fn []
+  `(~'fn []
      ~@(dlist p)))
--- code/embang/test/embang/core_test.clj
+++ code/embang/test/embang/core_test.clj
@@ -1,7 +1,3 @@
 (ns embang.core-test
   (:require [clojure.test :refer :all]
             [embang.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
