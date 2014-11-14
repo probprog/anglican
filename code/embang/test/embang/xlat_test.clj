965a5eba3d70e980fa8648c6c389a529835aaf99
David Tolpin <david.tolpin@gmail.com>
2014-11-14 15:12:17 +0000

    added forgotten xlat test

---
+++ code/embang/test/embang/xlat_test.clj
@@ -0,0 +1,53 @@
+(ns embang.xlat_test
+  (:require [clojure.test :refer [deftest testing is]])
+  (:use embang.xlat))
+
+(deftest test-elist
+  (testing "elist"
+    (is (= (elist '(1 2)) '(1 2)) "simple list")
+    (is (= (elist '((define x 1) x)) '((let [x 1] x))) "define in list")
+    (is (= (elist '((define y 2))) '((let [y 2]))) "define is last")))
+
+(deftest test-alambda
+  (testing "alambda"
+    (is (= (alambda '((a b c) a b c))
+           '(fn [a b c] a b c))
+        "simple")
+    (is (= (alambda '(a a))
+           '(fn [& a] a))
+        "variadic")))
+
+(deftest test-alet
+  (testing "alet"
+    (is (= (alet '(((x 1) (y 2)) (+ x y))) '(let [x 1 y 2] (+ x y))) "let")))
+
+(deftest test-acond
+  (testing "acond"
+    (is (= (acond '(((= x 1) x) ((= x 2) (* 2 x))))
+           '(cond (= x 1) x (= x 2) (* 2 x))) "without else")
+    (is (= (acond '(((= x 1) x) (else (* 2 x))))
+           '(cond (= x 1) x :else (* 2 x))) "with else")))
+
+(deftest test-abegin
+  (testing "abegin"
+    (is (= (abegin '((do 1) 2)) '(do (begin 1) 2)) "begin with name clash")))
+
+(deftest test-aform
+  (testing "aform"
+    (is (= (aform '(fn 1 2)) '(lambda 1 2)) "application with name clash")))
+
+(deftest test-expression
+  (testing "expression"
+    (is (= (expression '()) ()) "empty list")))
+
+(deftest test-dlist
+  (testing "dlist"
+    (is (= (dlist '([assume x 1]
+                    [assume y 2]
+                    [predict y]
+                    [observe (normal x 1) 1]))
+           '((let [x 1]
+               (let [y 2]
+                 (predict y)
+                 (observe (normal x 1) 1)))))
+        "assumes, predict, observe")))
