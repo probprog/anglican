f161bb30d4cdadfe15b465190e75399dbc4d60a0
David Tolpin <david.tolpin@gmail.com>
2014-11-17 00:39:31 +0000

    almost there

--- code/embang/src/embang/core.clj
+++ code/embang/src/embang/core.clj
@@ -1,10 +1,13 @@
 (ns embang.core
   (:gen-class)
   (:require [clojure.pprint :refer [pprint]])
-  (:use [embang.xlat :only [anglican]]))
+  (:use [embang.xlat :only [anglican]])
+  (:use [embang.trap :only [cps-of-expr]]))
 
 (defn -main
   "transforms anglican program to clojure code"
   [& args]
-  (pprint (anglican 
-            (read-string (str "(" (slurp *in*) ")")))))
+  (pprint (cps-of-expr
+           (anglican 
+            (read-string (str "(" (slurp *in*) ")")))
+           'return)))
--- code/embang/src/embang/trap.clj
+++ code/embang/src/embang/trap.clj
@@ -16,10 +16,29 @@
 ;;   - the list of predicted values.
 
 (declare cps-of-expr)
+(declare ^:dynamic *primitive-procedures*)
+
+(defn primitive-procedure?
+  "true if the procedure is primitive,
+  that is does not have a CPS form"
+  ;; assumes that primitive procedure
+  ;; symbols are never rebound locally
+  [procedure]
+  (*primitive-procedures* procedure))
 
 (defn simple-expr?
+  "true if expr has no continuation"
   [expr]
-  false)
+  (or (nil? expr)
+      (not (seq? expr))
+      (case (first expr)
+        quote true
+        (begin if cond do) (every? simple-expr? (rest expr))
+        let (let [[_ bindings & body] expr]
+              (and (every? simple-expr? (map second bindings))
+                   (every? simple-expr? body)))
+        (and (primitive-procedure? (first expr))
+             (every? simple-expr? (rest expr))))))
 
 (defn cps-of-elist
   [exprs cont]
@@ -30,14 +49,24 @@
                       ~(cps-of-elist rst cont)))
       (cps-of-expr fst cont))))
 
+;; Asserts in cps-of-fn, cps-of-let make sure
+;; primitive procedures are uniquely identifiable by
+;; their names. 
+
+;; Continuation is the first, rather than the last, parameter
+;; of a function to support functions with variable arguments.
+
 (defn cps-of-fn
   [args cont]
   (if (vector? (first args))
     (cps-of-fn `[nil ~@args] cont)
     (let [[name parms & body] args
-          fncont (gensym "cont")]
+          fncont (gensym "C")]
+      (assert (not-any? primitive-procedure? parms)
+              (str "primitive procedure name as parameter: "
+                   (some primitive-procedure? parms)))
       `(~cont (~'fn ~@(when name [name])
-                [~fncont ~parms]
+                [~fncont ~@parms]
                 ~(cps-of-elist body fncont))))))
 
 (defn cps-of-let
@@ -46,32 +75,44 @@
   (if (seq bindings)
     (let [[name value & bindings] bindings
           rest (cps-of-let `(~bindings ~@body) cont)]
+      (assert (not (primitive-procedure? name))
+              (str "primitive procedure name rebound: " name))
+      (assert (not (primitive-procedure? value))
+              (str "primitive procedure locally bound: " value))
       (if (simple-expr? value)
         `(~'let [~name ~value]
            ~rest)
         (cps-of-expr value
-                     (let [value (gensym "value")]
+                     (let [value (gensym "V")]
                        `(~'fn [~value]
                           (~'let [~name ~value]
                             ~rest))))))
     (cps-of-elist body cont)))
-  
-(defn cps-of-if
-  "transforms cond to cps"
-  [[cnd thn els] cont]
+
+(defmacro defn-with-named-cont 
+  [cps-of parms & body]
+  (let [cont (last parms)]
+    `(defn ~cps-of ~parms
+       (if (symbol? ~cont)
+         (do ~@body)
+         (let [~'named-cont (gensym "C")]
+           `(~~''let [~~'named-cont ~~cont]
+              ~(~cps-of ~@(butlast parms) ~'named-cont)))))))
+
+(defn-with-named-cont cps-of-if
+  [[cnd thn els :as args] cont]
   (if (simple-expr? cnd)
-    `(~'if ~cnd
-       ~(cps-of-expr thn cont)
-       ~(cps-of-expr els cont))
+      `(~'if ~cnd
+         ~(cps-of-expr thn cont)
+         ~(cps-of-expr els cont))
       (cps-of-expr cnd
-                   (let [cnd (gensym "cnd")]
+                   (let [cnd (gensym "I")]
                      `(~'fn [~cnd]
                         (if ~cnd
                           ~(cps-of-expr thn cont)
                           ~(cps-of-expr els cont)))))))
-                          
-(defn cps-of-cond
-  "transforms cond to cps"
+  
+(defn-with-named-cont cps-of-cond
   [clauses cont]
   (if clauses
     (let [[cnd thn & clauses] clauses]
@@ -91,6 +132,34 @@
   `(~'let [~'$predicts (~'conj ~'$predicts ['~expr ~expr])]
      (~cont nil)))
 
+
+(defn cps-of-alist
+  "cps of expanded argument list,  each entry in
+   the argument list  is a pair [expression substitution],
+   when expression is nil, it is simple and does not require
+   a continuation"
+  [alist])
+
+  
+(defn cps-of-application
+  "transforms application to cps"
+  [exprs cont]
+  (let [args (map (fn [expr]
+                    (if (simple-expr? expr) [nil expr] [expr (gensym "A")]))
+                  exprs)]
+    (letfn [(cps-of-alist [alist]
+              (if (seq alist)
+                (let [[[expr subst] & alist] alist]
+                  (if expr
+                    (cps-of-expr expr `(~'fn [~subst]
+                                         ~(cps-of-alist alist)))
+                    (cps-of-alist alist)))
+                (let [call (map second args)]
+                  (if (primitive-procedure? (first call))
+                    `(~cont ~call)
+                    `(~(first call) ~cont ~@(rest call))))))]
+      (cps-of-alist args))))
+
 (defn cps-of-expr
   [expr cont]
   (cond
@@ -99,21 +168,22 @@
      (let [[kwd & args] expr]
         (case kwd
           quote   `(~cont ~expr)
-          mem     `(~cont ~expr) ; TODO
           fn      (cps-of-fn args cont)
           let     (cps-of-let args cont)
           if      (cps-of-if args cont)
           cond    (cps-of-cond args cont)
           do      (cps-of-do args cont)
           predict (cps-of-predict args cont)
-          observe `(~cont ~expr) ; TODO
+          observe `(~'TODO ~cont ~expr)
+          sample  `(~'TODO ~cont ~expr)
+          mem     `(~'TODO ~cont ~expr)
           ;; application
-          `(~cont ~expr))) ; TODO
+          (cps-of-application expr cont)))
      :else `(~cont ~expr)))
 
-(def ^:const ^:private PRIMITIVE-FUNCTIONS
-  "primitive functions, do not exist in CPS form"
-  '[ ;; tests
+(def ^:dynamic *primitive-procedures*
+  "primitive procedures, do not exist in CPS form"
+  '#{ ;; tests
     boolean? symbol? string?   proc? number?
     ratio?  integer?  float?  even?  odd?
     nil?  some?  empty?  list?  seq?  
@@ -167,4 +237,4 @@
     ;; XRPs
     crp
     beta-flip
-    normal-with-known-std])
+    normal-with-known-std})
