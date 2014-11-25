f4fbb8ad9f3e00513d470b397baad40d159e66df
David Tolpin <david.tolpin@gmail.com>
2014-11-25 01:28:42 +0000

    smc resmapling in progress

--- code/embang/src/embang/importance.clj
+++ code/embang/src/embang/importance.clj
@@ -14,9 +14,9 @@
   #((:cont obs) nil (add-log-weight (:state obs)
                                     (observe (:dist obs) (:value obs)))))
 
-(defmethod infer :importance [_ prog & {:keys [number-of-samples output-format]
-                                        :or {number-of-samples -1
-                                             output-format :clojure}}]
+(defmethod infer :importance [_ prog & {:keys [number-of-samples
+                                               output-format]
+                                        :or {output-format :clojure}}]
   (loop [i 0]
     (when-not (= i number-of-samples)
       (print-predicts (:state (exec ::algorithm prog nil initial-state))
--- code/embang/src/embang/smc.clj
+++ code/embang/src/embang/smc.clj
@@ -14,32 +14,54 @@
   (update-in obs [:state]
              add-log-weight (observe (:dist obs) (:value obs))))
 
+(declare resample)
+
+(defn smc-sweep
+  "a single SMC sweep, can be called from other algorithms"
+  [algorithm prog number-of-particles]
+  (loop [particles (repeatedly number-of-particles
+                               #(exec algorithm prog nil initial-state))]
+    (cond
+     (every? #(instance? embang.trap.observe %) particles)
+     (recur (map #(exec algorithm (:cont %) nil (:state %))
+                 (resample particles)))
+
+     (every? #(instance? embang.trap.result %) particles)
+     particles
+     
+     :else (throw (AssertionError.
+                   "some `observe' directives are not global")))))
+
 ;; SMC produces all of its samples in parallel,
 ;; therefore the default number of samples cannot
 ;; be infinity. We arbitrarily set it to 1.
 
-(declare resample)
-
-;; TODO: smc-sweep, smc calls smc-sweep, smc-sweep parameterized by algorithm
-
-(defmethod infer :smc [_ prog & {:keys [number-of-samples output-format]
-                                 :or {number-of-samples 1 
+(defmethod infer :smc [_ prog & {:keys [number-of-samples
+                                        number-of-sweeps
+                                        number-of-particles
+                                        output-format]
+                                 :or {number-of-samples 1
                                       output-format :clojure}}]
-
-  (loop [particles (repeatedly number-of-samples #(exec ::algorithm prog nil initial-state))]
-    (cond
-      (every? #(instance? embang.trap.observe %) particles)
-      (recur (map #(exec ::algorithm (:cont %) nil (:state %))
-                  (resample particles)))
-
-      (every? #(instance? embang.trap.result %) particles)
-      (doseq [res (resample particles)]
-        (do (print-predicts (:state res) output-format)
-            particles))
-
-      :else (throw (AssertionError. "some `observe' directives are not global")))))
+  (let [number-of-sweeps (or number-of-sweeps
+                             (when number-of-samples 1))
+        number-of-particles (or number-of-particles
+                                number-of-samples)]
+    (dotimes [_ number-of-sweeps]
+      (doseq [res (smc-sweep ::algorithm prog number-of-particles)]
+        (print-predicts (:state res) output-format)))))
 
 (defn resample
   "resamples particles proportionally to their current weights"
   [particles]
-  particles)
+  (let [log-weights (map (comp get-log-weight :state) particles)
+        max-log-weight (max log-weights)
+        weights (map #(Math/exp (- % max-log-weight)) log-weights)
+        total-weight (reduce + weights)
+        step (/ total-weight (count particles))]
+    (loop [x (rand total-weight)
+           old-weights weights
+           old-particles particles
+           new-particles []]
+      particles)))
+
+        
