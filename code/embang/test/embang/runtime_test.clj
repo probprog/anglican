b41b3af7c9300d96c1528f80657b01acd860ed29
David Tolpin <david.tolpin@gmail.com>
2014-11-29 01:26:45 +0000

    tests for higher-order functions

---
+++ code/embang/test/embang/runtime_test.clj
@@ -0,0 +1,34 @@
+(ns embang.runtime-test
+  (:require [clojure.test :refer [deftest testing is]])
+  (:use [embang.trap :only [value-cont]]
+        embang.runtime))
+
+(deftest test-high-order-functions
+  (testing "map in CPS"
+    (is (= (trampoline
+            ($map value-cont nil (fn [cont $state lst]
+                                   (cont (first lst) $state))
+                  '((1) (2) (3))))
+           '(1 2 3))
+        "map on a single list")
+    (is (= (trampoline
+            ($map value-cont nil (fn [cont $state x y]
+                                   (cont (+ x y) $state))
+             '( 1 2 3) '(4 5 6)))
+           '(5 7 9))
+        "map on two lists"))
+
+  (testing "reduce in CPS"
+    (is (= (trampoline
+            ($reduce value-cont nil (fn [cont $state x y]
+                                      (cont (+ x y) $state))
+                    '(1 2 3)))
+           6)
+        "reduce without default")
+    (is (= (trampoline
+            ($reduce value-cont nil (fn [cont $state x y]
+                                      (cont(conj x y) $state))
+                     nil
+                     '(1 2 3)))
+           '(3 2 1))
+        "reduce with default")))
