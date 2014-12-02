b3e5ae08444dae48164e88e3c91538461a339e21
David Tolpin <david.tolpin@gmail.com>
2014-12-02 01:35:30 +0000

    resolved degeneracy of numerical ops

--- code/src/embang/smc.clj
+++ code/src/embang/smc.clj
@@ -49,51 +49,58 @@
 ;; Systematic resampling is used. The particles are assigned
 ;; unit weight after resampling.
 
+(defn ^:private recover-weights
+  "recovers weights from log-weights"
+  [log-weights]
+  (let [max-log-weight (reduce max log-weights)]
+    (map (fn [log-weight]
+           ;; avoid NaN and Infinity
+           (cond
+            (= log-weight max-log-weight) 1.
+            (= log-weight (Math/log 0.)) 0.
+            :else (Math/exp (- log-weight max-log-weight))))
+         log-weights)))
+
 (defn resample
   "resamples particles proportionally to their current weights"
   ([particles] (resample particles (count particles)))
   ([particles number-of-new-particles]
-   (let [log-weights (map (comp get-log-weight :state) particles)
-         max-log-weight (reduce max log-weights)
-         weights (map #(Math/exp (- % max-log-weight)) log-weights)
-         total-weight (reduce + weights)]
 
-     (if (= total-weight 0.) particles   ; all particles have
-       ; the same weight
-       ;;; Systematic sampling
+     ;; invariant bindings for sampling
+     (let [weights (recover-weights
+                    (map (comp get-log-weight :state) particles))
+           total-weight (reduce + weights)
+           step (/ total-weight number-of-new-particles)
+           all-weights weights     ; particle sequence is circular
+           all-particles particles]
 
-       ;; invariant bindings for sampling
-       (let [step (/ total-weight number-of-new-particles)
-             all-weights weights     ; particles are circular
-             all-particles particles]
+       ;; the systematic sampling loop
+       (loop [x (rand total-weight)
+              n 0      ; number of particles sampled so far
+              acc 0    ; upper bound of the current segment
+              weights all-weights
+              particles all-particles
+              new-particles nil]
+         (if (= n number-of-new-particles)
+           new-particles
+           (let [[weight & next-weights] weights
+                 [particle & next-particles] particles
+                 next-acc (+ acc weight)]
 
-         (loop [x (rand total-weight)
-                n 0      ; number of particles sampled so far
-                acc 0    ; upper bound of the current segment
-                weights weights
-                particles particles
-                new-particles nil]
-           (if (= n number-of-new-particles)
-             new-particles
-             (let [[weight & next-weights] weights
-                   [particle & next-particles] particles
-                   next-acc (+ acc weight)]
-               (if (< x next-acc)
-
-                 ;; Found the wheel segment into which x has fallen.
-                 ;; Advance x by step for the next particle's segment.
-                 (recur (+ x step) (+ n 1) 
-                        acc weights particles
-                        (conj new-particles
-                              (update-in
-                                particle [:state]
-                                set-log-weight 0.)))
-
-                 ;; Otherwise, keep going through the particle's 
-                 ;; segments, recycling the list of particles and
-                 ;; their weights when necessary.
-                 (recur x n
-                        next-acc
-                        (or next-weights all-weights)
-                        (or next-particles all-particles)
-                        new-particles))))))))))
+             (if (< x next-acc)
+               ;; Found the wheel segment into which x has fallen.
+               ;; Advance x by step for the next particle's segment.
+               (recur (+ x step) (+ n 1) 
+                      acc weights particles
+                      (conj new-particles
+                            (update-in
+                             particle [:state]
+                             set-log-weight 0.)))
+               ;; Otherwise, keep going through the particle's 
+               ;; segments, recycling the list of particles and
+               ;; their weights when necessary.
+               (recur x n
+                      next-acc
+                      (or next-weights all-weights)
+                      (or next-particles all-particles)
+                      new-particles))))))))
