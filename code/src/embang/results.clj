0feb76d60777c253a75f02d552421a43486364b9
David Tolpin <david.tolpin@gmail.com>
2014-12-08 12:37:55 +0000

    in progres, broken

--- code/src/embang/results.clj
+++ code/src/embang/results.clj
@@ -1,6 +1,8 @@
 (ns embang.results
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.data.json :as json]))
 
 (defmacro redir
   "Input/output redirection macro,
@@ -15,24 +17,60 @@
   and the output is appended to the file."
   [[& {:keys [in out] :as args}]  & body]
   (cond
-    in 
-    (let [rdr (gensym "rdr")]
-      `(with-open [~rdr (io/reader ~in)]
-         (binding [*in* ~rdr]
-           (redir [~@(flatten (seq (dissoc args :in)))]
-                  ~@body))))
+   in 
+   (let [rdr (gensym "rdr")]
+     `(with-open [~rdr (io/reader ~in)]
+        (binding [*in* ~rdr]
+          (redir [~@(flatten (seq (dissoc args :in)))]
+                 ~@body))))
 
-    out
-    (let [wtr (gensym "wtr")]
-      `(with-open [~wtr ~(if (= (subs out 0 1) "+")
-                           `(io/writer ~(subs out 1) :append true)
-                           `(io/writer ~out))]
-         (binding [*out* ~wtr]
-           (redir [~@(flatten (seq (dissoc args :out)))]
-                  ~@body))))
+   out
+   (let [wtr (gensym "wtr")]
+     `(with-open [~wtr ~(if (= (subs out 0 1) "+")
+                          `(io/writer ~(subs out 1) :append true)
+                          `(io/writer ~out))]
+        (binding [*out* ~wtr]
+          (redir [~@(flatten (seq (dissoc args :out)))]
+                 ~@body))))
 
-    :else
-    `(do ~@body)))
+   :else
+   `(do ~@body)))
+
+(defmulti parse-line 
+  "parses output line and returns [label value height]"
+  (fn [format line] format))
+
+;; legacy anglican syntax
+(defmethod parse-line :anglican [_ line]
+  (let [fields (str/split line #" *, *")
+        [label value weight] (map edn/read-string
+                                  (take-last 3 fields))]
+    [label value (or weight 1.)]))
+
+(defmethod parse-line :clojure [_ line]
+  (edn/read-string line))
+
+(defmethod parse-line :json [_ line]
+  (json/read-str line))
+
+(defn parsed-line-seq
+  "parses line sequence"
+  ([lines] (parsed-line-seq lines :anglican))
+  ([lines format]
+     (println "FORMAT:" format)
+     (when-let [[line & lines] (seq lines)]
+       (println "LINE:" line)
+       (if (re-find #"^\s*;" line)
+         ;; meta-info or comment
+         (if-let [format-option (re-matches
+                                 #"^;;\s*:output-format\s+:(.+)"
+                                 line)]
+           (do
+             (println "format-option" format-option)
+             (recur lines (keyword (format-option 1))))
+           (recur lines format))
+         (cons (parse-line format line)
+               (lazy-seq (parsed-line-seq lines format)))))))
 
 (defn freqs
   "reads the result file in anglican format
@@ -40,30 +78,24 @@
   integer-valued predict"
   ([iname] (redir [:in iname] (freqs)))
   ([]
-   (loop [lines (line-seq (io/reader *in*))
-          weights {}]
-     (if (seq lines)
-       (let [[line & lines] lines
-             line (str/trim line)]
-         (if (= (subs line 0 1) ";")
-           (recur lines weights) ; comment line
-           (let [fields (str/split line #" *, *")
-                 label (str/join "," (subvec fields
-                                             0 (- (count fields) 2)))
-                 [value weight] (map read-string
-                                     (subvec fields (- (count fields) 2)))]
-             (recur 
-               lines
-               (if (or (integer? value) (symbol? value))
-                 (update-in weights [label value] (fnil + 0.) weight)
-                 weights)))))
-       (doseq [label (sort (keys weights))]
-         (let [total-weight (reduce + (vals (weights label)))]
-           (doseq [value (sort (keys (weights label)))]
-             (let [weight (/ (double (get-in weights [label value]))
-                             total-weight)]
-               (printf "%s, %s, %6g, %6g\n"
-                       label value weight (Math/log weight))))))))))
+     (loop [lines (parsed-line-seq (line-seq (io/reader *in*)))
+            weights {}]
+       (if (seq lines)
+         (let [[[label value weight] & lines] lines]
+           (println "PARSED LINE: " label value weight)
+           (recur 
+            lines
+            (if (or (integer? value) (symbol? value))
+              (update-in weights [label value] (fnil + 0.) weight)
+              weights)))
+
+         (doseq [label (sort (keys weights))]
+           (let [total-weight (reduce + (vals (weights label)))]
+             (doseq [value (sort (keys (weights label)))]
+               (let [weight (/ (double (get-in weights [label value]))
+                               total-weight)]
+                 (printf "%s, %s, %6g, %6g\n"
+                         label value weight (Math/log weight))))))))))
 
 (defn meansd
   "reads the result file in anglican format
@@ -71,26 +103,19 @@
   for each predict"
   ([iname] (redir [:in iname] (freqs)))
   ([]
-     (loop [lines (line-seq (io/reader *in*))
+     (loop [lines (parsed-line-seq (line-seq (io/reader *in*)))
             sums {}]
        (if (seq lines)
-         (let [[line & lines] lines
-               line (str/trim line)]
-           (if (= (subs line 0 1) ";")
-             (recur lines sums) ; comment line
-             (let [fields (str/split line #" *, *")
-                   label (str/join "," (subvec fields
-                                               0 (- (count fields) 2)))
-                   [value weight] (map read-string
-                                       (subvec fields (- (count fields) 2)))]
-               (recur 
-                lines
-                (let [weighted-value (* value weight)]
-                  (-> sums
-                      (update-in [label :weight] (fnil + 0.) weight)
-                      (update-in [label :sum] (fnil + 0.) weighted-value)
-                      (update-in [label :sum2] (fnil + 0.) 
-                                 (* weighted-value weighted-value))))))))
+         (let [[[label value weight] & lines] lines]
+           (recur 
+            lines
+            (let [weighted-value (* value weight)]
+              (-> sums
+                  (update-in [label :weight] (fnil + 0.) weight)
+                  (update-in [label :sum] (fnil + 0.) weighted-value)
+                  (update-in [label :sum2] (fnil + 0.) 
+                             (* weighted-value weighted-value))))))
+
          (doseq [label (sort (keys sums))]
            (let [mean (/ (get-in sums [label :sum])
                          (get-in sums [label :weight]))
