aa5aa684e3f2f3036d09f4f621e882e287c04d58
David Tolpin <david.tolpin@gmail.com>
2015-01-07 22:05:06 +0000

    map in progress

--- code/src/embang/map.clj
+++ code/src/embang/map.clj
@@ -1,5 +1,6 @@
 (ns embang.map
   (:require [embang.colt.distributions :as dist])
+  (:require [clojure.data.priority.map :refer []])
   (:use [embang.state :exclude [initial-state]]
         [embang.runtime :only [sample observe]]
         embang.inference))
@@ -26,101 +27,44 @@
   (bb-update [belief evidence]
     "updates belief based on the evidence")
   (bb-sample [belief]
-    "returns a random sample from the belief distribution")
-  (bb-mode [belief]
-    "returns the mode of the distribution"))
+    "returns a random sample from the belief distribution"))
 
-;;;; Mean reward belief, via Gamma distribution
+;;;; Mean reward belief
 
 (defn mean-reward-belief
   [shape rate]
   ;; Bayesian belief about the mean reward  (log-weight).
-  ;; 
-  ;; We use log-Gamma belief for rewards and impose 
-  ;; Gamma prior on the rate of the reward distribution.
-  (let [distribution (dist/gamma-distribution shape rate)]
+  (let [distribution ]
     (reify bayesian-belief
       (bb-update [mr reward]
-        (mean-reward-belief (+ shape 1.) (+ rate (Math/exp reward))))
-      ;; Since  the mean reward distributions are used for
-      ;; comparison only, the absolute value of the shape of
-      ;; the reward distribution does not matter, provided
-      ;; it is assumed to be the same for all arms.
+        )
       (bb-sample [mr]
-        (Math/log (/ (dist/draw distribution))))
-      (bb-mode [mr]
-        (Math/log (/ rate (- shape 1.)))))))
-
-(def initial-mean-reward-belief
-  "initial belief about mean reward" 
-  ;; Uninformative prior --- we know nothing about
-  ;; the absolute values of rewards in general.
-  (mean-reward-belief 1. Double/MIN_NORMAL))
+        ))))
 
 ;;;; Bandit
 
 ;; selects arms using randomized probability matching
 
-;; Best arm is used both for randomized selection, 
-;; scored by bb-sample, and for the final arm selection,
-;; scored by bb-mode
-
 (defn best-arm
   "select an arm with the best core"
-  [arms bb-score best-score best-arm]
-  (if-let [[[value belief :as arm] & arms] (seq arms)]
-    (let [score (bb-score belief)]
+  [arms best-score best-arm]
+  (if-let [[[_ belief :as arm] & arms] (seq arms)]
+    (let [score (bb-sample belief)]
       (if (>= score best-score)
-        (recur arms bb-score score arm)
-        (recur arms bb-score best-score best-arm)))
+        (recur arms score arm)
+        (recur arms best-score best-arm)))
     best-arm))
 
-(defn select-most-promising-arm
-  "selects an arm by sampling from mean's belief,
-  returns the value of an existing arm,
-  or nil if a new value is to be drawn"
-  [arms]
-  ;; First, select an arm which belief will be used
-  ;; for a bet of the new arm.
-  (when-let [[_ belief] (best-arm arms bb-sample
-                                  Double/NEGATIVE_INFINITY nil)]
-    ;; Then, select an arm with the best bet; if the bet
-    ;; of a new arm is the best, add a new arm.
-    (when-let [[value _] (best-arm arms bb-sample
-                                   (bb-sample belief) nil)]
-      value)))
-
-(defn select-maximum-a-posteriori-arm
-  "returns the value of an arm with
-  the highest mode of the belief,
-  or nil if there are no arms"
-  [arms]
-  (when-let [[value _] (best-arm arms bb-mode
-                                  Double/NEGATIVE_INFINITY nil)]
-    value))
-
 (defn update-arm 
-  "updates the belief about arm in arms"
-  [arms arm reward]
+  "updates the belief about arm in arms,
+  uses prior-belief for a new arm"
+  [arms prior-belief arm reward]
   (update-in arms [arm]
-             (fnil bb-update initial-mean-reward-belief) reward))
+             (fnil bb-update prior-belief) reward))
 
 ;;;; MAP inference
 
 ;;; Random choice bandit
-
-;; A bandit is in either default or frozen `mood.'  When
-;; a bandit is in the default mood, the arm is selected by
-;; sampling from the mean's belief. When the bandit is frozen,
-;; the arm with the maximum probability mass is selected.
-
-(defmulti select-arm :mood)
-
-(defmethod select-arm :frozen [bandit]
-  (select-maximum-a-posteriori-arm (:arms bandit)))
-
-(defmethod select-arm :default [bandit]
-  (select-most-promising-arm (:arms bandit)))
 
 (defn update-bandit
   "updates bandit's belief"
@@ -128,16 +72,6 @@
   (-> bandit
       (update-in [:arms] (fnil update-arm {}) sample reward)
       (update-in [:count] (fnil inc 0))))
-
-(defn freeze-bandit
-  "sets the bandit's mood to frozen"
-  [bandit]
-  (assoc bandit :mood :frozen))
-
-(defn frozen?
-  "true when the bandit is frozen"
-  [bandit]
-  (= (:mood bandit) :frozen))
 
 ;;; State transformations
 
@@ -167,22 +101,25 @@
         (assoc initial-state
                ::bandits (freeze bandits can-be-frozen?))))))
 
-(defn maximum-a-posteriori
-  "given the state, returns a vector of
-  maximum a posteriori samples"
-  [state]
-  (map second (state ::trace)))
+;;; Trace
+
+;; the trace is a vector of tuples
+;;   [bandit-id value past-reward]
+;; where past reward is the reward accumulated 
+;; before reaching this random choice
+
 
 ;; Bandit id: different random choices should get different
 ;; ids, ideally structurally similar random choices should
 ;; get the same id, just like addresses in Random DB
 (defn bandit-id [smp trace]
-  "returns bandit id for the checkpoint,
-  the id includes the complete trace prefix"
+  "returns bandit id for the checkpoint"
   [(:id smp)  ; static identifier of the random choice
    (count     ; number of preceding draws from the same choice
-     (filter (fn [[[smp-id _] & _]] (= smp-id (:id smp)))
+     (filter (fn [entry] (= smp-id (:id smp)))
              trace))])
+
+
 
 (defmethod checkpoint [::algorithm embang.trap.sample] [algorithm smp]
   (let [state (:state smp)
