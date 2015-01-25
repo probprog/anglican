3b6100582660c70f077be6ba37028a4dcf8ec092
David Tolpin <david.tolpin@gmail.com>
2015-01-25 13:10:07 +0000

    analysis in progress

--- code/src/embang/analysis.clj
+++ code/src/embang/analysis.clj
@@ -1,12 +1,18 @@
-(ns embang.analysis)
+(ns embang.analysis
+  (require [clojure.java.io :as io])
+  (use [embang.results :only [parsed-line-seq
+                              total-weights
+                              normalize-weights]]))
 
-;;; Analysis of inference results.
+;;;; Analysis of inference results.
+
+;;; Sample distance measures.
 
 ;; For use with KS-two-samples.
 (defn search-sorted 
-  "returns a sequence of indices such that if values are
-  inserted at the indices into grid, grid remains sorted;
-  assumes that both grid and values are sorted"
+  "returns a sequence of indices such that if `values' are
+  inserted at the indices into `grid', `grid' remains sorted;
+  assumes that both `grid' and `values' are sorted"
   [grid values]
   (letfn
     [(indices [grid values index]
@@ -19,7 +25,7 @@
              (indices (rest grid) values (inc index))))))]
     (indices grid values 0)))
 
-(defn KS-two-samples
+(defn KS
   "computes Kolmogorov-Smirnov distance for two samples"
   [sa sb]
   (let [sa (sort sa)
@@ -31,21 +37,57 @@
     (reduce max (map #(Math/abs (- %1 %2)) (cdf sa) (cdf sb)))))
   
 (defn KL
-  "computes Kullback-Leibler divergence for counts"
-  [p-counts q-counts] {:pre [(map? p-counts) (map? q-counts)]}
-  ;; Fix counts so that have the same keys.
-  (let [fix-counts (fn [a-counts b-counts]
+  "computes Kullback-Leibler divergence for value frequencies."
+  [p-freqs q-freqs] {:pre [(map? p-freqs) (map? q-freqs)]}
+  ;; Fix freqs so that have the same keys.
+  (let [fix-freqs (fn [a-freqs b-freqs]
                      (reduce (fn [ac k]
                                (if (ac k) ac
                                  (assoc ac k Double/MIN_NORMAL)))
-                             a-counts (keys b-counts)))
-        p-counts (fix-counts p-counts q-counts)
-        q-counts (fix-counts q-counts p-counts)
-        PC (double (reduce + (vals p-counts)))
-        QC (double (reduce + (vals q-counts)))]
-    (prn p-counts q-counts)
+                             a-freqs (keys b-freqs)))
+        p-freqs (fix-freqs p-freqs q-freqs)
+        q-freqs (fix-freqs q-freqs p-freqs)
+        PC (double (reduce + (vals p-freqs)))
+        QC (double (reduce + (vals q-freqs)))]
+    (prn p-freqs q-freqs)
     (reduce (fn [kl k]
-              (let [q (/ (q-counts k) QC)
-                    p (/ (p-counts k) PC)]
+              (let [q (/ (q-freqs k) QC)
+                    p (/ (p-freqs k) PC)]
                 (+ kl (* p (Math/log (/ p q))))))
-            0. (keys q-counts))))
+            0. (keys q-freqs))))
+
+;; REPL command:
+(defn total-freqs
+  "reads results from stdin and returns
+  a table of total frequences for discrete-valued predicts"
+ []
+ (normalize-weights (total-weights)))
+
+;; REPL command:
+(defn kl-seq 
+  "reads results from stdin and returns a lazy sequence
+  of KL distances, skipping first `skip' predict lines and
+  then producing a sequence entry each `step' predict lines"
+  [true-freqs skip step]
+  ;; TODO
+  )
+
+;; REPL command:
+(defn total-predicts
+  "reads results from stdin and returns a lazy sequence of predicts,
+  skipping first `skip' predict lines and then producing a sequence
+  entry each `step' predict lines"
+  [skip step]
+  (map (fn [[predict]] predict)
+    (partition 1 step
+               (drop skip
+                     (parsed-line-seq (line-seq (io/reader *in*))))))) 
+
+;; REPL command:
+(defn ks-seq
+  "reads results from stdin and returns a lazy sequence
+  of KS distances, skipping first `skip' predict lines and
+  then producing a sequence entry each `step' predict lines"
+  [true-samples skip step]
+  ;; TODO
+  )
