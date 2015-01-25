1fcc9c2772cd08be88f077327250fdb5abfc1a64
David Tolpin <david.tolpin@gmail.com>
2015-01-25 00:48:41 +0000

    analysis scripts in progress

--- code/src/embang/analysis.clj
+++ code/src/embang/analysis.clj
@@ -1,3 +1,32 @@
 (ns embang.analysis)
 
 ;;; Analysis of inference results.
+
+(defn search-sorted 
+  "returns a sequence of indices such that if values are
+  inserted at the indices into grid, grid remains sorted"
+  [grid values]
+  (letfn
+    [(indices [grid values last-index next-index]
+       (lazy-seq 
+         (when (seq values)
+           (cond
+             (empty? grid)
+             (if (< last-index next-index) 
+               (cons next-index nil)
+               nil)
+
+             (> (first grid) (first values))
+             (let [more-indices (indices grid (rest values)
+                                         next-index next-index)]
+               (if (< last-index next-index)
+                 (cons next-index more-indices)
+                 more-indices))
+
+             :else
+             (indices (rest grid) values
+                      last-index (inc next-index))))))]
+    (indices grid values -1 0)))
+
+
+
