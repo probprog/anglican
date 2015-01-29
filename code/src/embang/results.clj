ff6646c96f983e21feb1f79392e3bdc5e8bf319f
David Tolpin <david.tolpin@gmail.com>
2015-01-29 11:32:35 +0000

    diff merged into m!, new command-line syntax

--- code/src/embang/results.clj
+++ code/src/embang/results.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [clojure.edn :as edn]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.tools.cli :as cli]))
 
 ;;;; Helper commands for inference results
 
@@ -83,7 +84,6 @@
 ;;; Summary statistics on inference results
 
 ;; Discrete results: value frequences (categorical distribution).
-
 ;; Helper functions: total-weights and normalize-weights.
 
 (defn total-weights
@@ -154,3 +154,247 @@
                                  (get-in sums [label :weight]))
                                  (* mean mean)))]
           (printf "%s, %6g, %6g\n" label mean sd))))))
+
+;;; Sample distance measures
+
+(defn KL
+  "computes Kullback-Leibler divergence for value frequencies."
+  [p-freqs q-freqs] {:pre [(map? p-freqs) (map? q-freqs)]}
+  ;; Fix freqs so that p and q have the same keys.
+  (reduce (fn [kl k]
+            (let [q (q-freqs k)
+                  p (p-freqs k)]
+              (if (and p q)
+                (+ kl (* p (Math/log (/ p q))))
+                kl)))
+          0. (keys q-freqs)))
+
+;; For use with KS-two-samples:
+(defn search-sorted
+  "returns a sequence of indices such that if `values' are
+  inserted at the indices into `grid', `grid' remains sorted;
+  assumes that both `grid' and `values' are sorted"
+  [grid values]
+  (letfn
+    [(indices [grid values index]
+       (lazy-seq
+         (when (seq values)
+           (if (or (empty? grid)
+                   (> (first grid) (first values)))
+             (cons index
+                   (indices grid (rest values) index))
+             (indices (rest grid) values (inc index))))))]
+    (indices grid values 0)))
+
+(defn KS
+  "computes Kolmogorov-Smirnov distance for two samples"
+  [sa sb]
+  (let [sa (sort sa)
+        sb (sort sb)
+        s (sort (concat sa sb))
+        cdf (fn [sx]
+              (let [nx (double (count sx))]
+                (map #(/ (double %) nx) (search-sorted sx s))))]
+    (reduce max (map #(Math/abs (- %1 %2)) (cdf sa) (cdf sb)))))
+
+;;; Manipulating inference outputs
+
+(defn included?
+  "true when the label should be included"
+  [only exclude label]
+  (and (or (empty? only) (contains? only label))
+       (not (contains? exclude label))))
+
+(defn predict-seq-skipping
+  "returns lazy sequence of predicts, 
+  skipping first `skip' predicts"
+  [skip]
+  (drop skip (parsed-line-seq (line-seq (io/reader *in*)))))
+
+;; REPL command:
+(defn total-freqs
+  "reads results from stdin and returns
+  a table of total frequences for discrete-valued predicts"
+  [& {:keys [only exclude]
+      :or {only nil
+           exclude #{}}}]
+  (let [total-weights (total-weights)]
+    (normalize-weights
+      (reduce dissoc total-weights 
+              (keep (complement 
+                      (partial included?  only exclude))
+                    (keys total-weights))))))
+
+;; REPL command:
+(defn kl-seq
+  "reads results from stdin and returns a lazy sequence
+  of KL distances, skipping first `skip' predict lines and
+  then producing a sequence entry each `step' predict lines"
+  [true-freqs & {:keys [skip step only exclude]
+                 :or {skip 0
+                      step 1
+                      only nil
+                      exclude #{}}}]
+  (letfn
+    [(kl-seq* [lines nlines weights]
+       (lazy-seq
+         (if (empty? lines) nil
+           (let [[[label value weight] & lines] (seq lines)
+                 weights (if (included? only exclude label)
+                           (update-in weights [label value]
+                                      (fnil + 0.) weight)
+                           weights)]
+             (if (= nlines step)
+               ;; After each `step' predict lines, include KL
+               ;; into the sequence.
+               (cons
+                 (let [freqs (normalize-weights weights)]
+                   (reduce
+                     + (map (fn [label]
+                              (KL (true-freqs label) (freqs label)))
+                            (keys true-freqs))))
+                 (kl-seq* lines 1 weights))
+               ;; Otherwise, just accumulate the weights.
+               (kl-seq* lines (inc nlines) weights))))))]
+    (kl-seq* (predict-seq-skipping skip) 1 {})))
+
+;; REPL command:
+(defn total-samples
+  "reads results from stdin and returns a map label -> sequence
+  of samples, skipping first `skip' predict lines and
+  then processing one entry per label each `step' predict lines"
+  [& {:keys [skip step only exclude]
+      :or {skip 0
+           step 1
+           only nil
+           exclude #{}}}]
+  (loop [predicts (predict-seq-skipping skip)
+         nlines 1
+         samples {}
+         seen-labels #{}]
+    (if-let [[[label value _] & predicts] (seq predicts)]
+      (let [samples (if (and (included? only exclude label)
+                             (not (contains? seen-labels label)))
+                      (update-in samples [label]
+                                 (fnil conj []) value)
+                      samples)]
+        (if (= nlines step)
+          ;; After each nlines forget seen labels
+          ;; and start collecting new layer of samples.
+          (recur predicts 1
+                 samples (empty seen-labels))
+          ;; Only a single value for every label is
+          ;; collected over each nlines.
+          (recur predicts (inc nlines)
+                 samples (conj seen-labels label))))
+      samples)))
+
+;; REPL command:
+(defn ks-seq
+  "reads results from stdin and returns a lazy sequence
+  of KS distances, skipping first `skip' predict lines and
+  then producing a sequence entry each `step' predict lines"
+  [true-samples & {:keys [skip step only exclude]
+                   :or {skip 0
+                        step 1
+                        only nil
+                        exclude #{}}}]
+  (letfn
+    [(ks-seq* [lines nlines samples]
+       (lazy-seq
+       (if (empty? lines) nil
+         (let [[[label value _] & lines] (seq lines)
+               samples (if (included? only exclude label)
+                         (update-in samples [label]
+                                    (fnil conj []) value)
+                         samples)]
+           (if (= nlines step)
+             ;; After each `step' predict lines, include KS
+             ;; into the sequence.
+             (cons (reduce
+                     + (map (fn [label]
+                              (KS (true-samples label)
+                                  (samples label)))
+                            (keys true-samples)))
+                   (ks-seq* lines 1 samples))
+               ;; Otherwise, just collect the samples.
+               (ks-seq* lines (inc nlines) samples))))))]
+    (ks-seq* (predict-seq-skipping skip) 1 {})))
+
+;;; Diff: difference between prediction and truth
+
+;; Command-line utility that takes the configuration and outputs of
+;; the distance for a single experiment outcome and the truth.
+;; 
+;; The truth is stored in an external file in predict format. So,
+;; the configuration would look like
+
+#_ (:period 19                          ; number of predicts per particle
+    :only [(get-state 1) (get-state 2)] ; predicts to account for
+    :exclude []                         ; predicts to ignore
+    :distance :ks                       ; type of distance
+    :truth "hhmm.truth")                ; resource containing the truth
+
+;; Two additional parameters, skip and step, 
+;; are provided on the command line.
+
+(def cli-options
+  [;; problems
+   ["-s" "--skip N" "Skip first N predict lines"
+    :default 0
+    :parse-fn #(Integer/parseInt %)]
+   ["-t" "--step N" "Output distance each N predict lines"
+    :default 1
+    :parse-fn #(Integer/parseInt %)]
+   ["-h" "--help" "print usage summary and exit"]])
+
+(defn usage [summary]
+  (str "Usage:
+     lein run conf.edn [option ...] < results > distances
+
+Options:
+" summary))
+
+(defn error-msg [errors]
+  (str/join "\n\t" (cons "ERROR parsing the command line:" errors)))
+
+(defn diff
+  "command-line/REPL utility that takes problem configuration 
+  and inference output and produces differences between
+  the input and the truth"
+  [& args]
+  (let [{:keys [options arguments errors summary] :as parsed-options}
+        (cli/parse-opts args cli-options)]
+
+    ;; Handle help and error conditions.
+    (cond
+      (:help options) (binding [*out* *err*]
+                        (println (usage summary)))
+
+      errors (binding [*out* *err*]
+               (println (error-msg errors)))
+
+      (empty? arguments) (binding [*out* *err*]
+                           (println (usage summary)))
+
+      :else
+      (let [config (apply hash-map 
+                          (with-open [in (java.io.PushbackReader.
+                                           (io/reader
+                                             (io/resource (first arguments))))]
+                            (edn/read in)))]
+        (binding [*out* *err*]
+          (doseq [[option value] (sort-by first (merge options config))]
+            (printf ";; %s %s\n" option value)))
+        (let [[mk-seq get-truth] (case (:distance config)
+                                   :kl [kl-seq total-freqs]
+                                   :ks [ks-seq total-samples])
+              truth (redir [:in (io/resource (:truth config))]
+                      (get-truth))
+              period (or (:period config) 1)]
+          (doseq [distance (mk-seq truth
+                                   :skip (* (:skip options) period)
+                                   :step (* (:step options) period)
+                                   :only (set (:only config))
+                                   :exclude (set (:exclude config)))]
+            (prn distance)))))))
