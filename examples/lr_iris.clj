c4a667f76aafe035365e5dbead8d3384b13af0d5
David Tolpin <david.tolpin@gmail.com>
2015-02-06 02:52:25 +0000

    new lr-iris

--- examples/lr_iris.clj
+++ examples/lr_iris.clj
@@ -1,29 +1,37 @@
 (ns lr-iris
+  (:require [clojure.core.matrix :refer [dot]])
   (:use [embang runtime emit]
         [angsrc iris-data]))
 
-(defanglican lr-iris
-  [assume dot-product
-   (lambda (u v) 
-     (if (= (count u) 0) 0 
-       (+ (* (first u) (first v))
-          (dot-product (rest u) (rest v)))))]
-  [assume sigma (sqrt (sample (gamma 1 1)))]
-  [assume b (repeatedly
-              5 (lambda () (sample (normal 0. sigma))))]
-  [assume z (lambda (x) 
-              (/ 1. (+ 1. (exp (* -1. (dot-product b x))))))]
-  (reduce (lambda (_ record)
-            (observe (flip (z (cons 1 (butlast record))))
-                     (= (last record) iris-setosa)))
-          () iris-data)
+(def iris-data-setosa (filter #(= (last %) :setosa) iris-data))
+(def iris-data-not-setosa (filter #(= (last %) :setosa) iris-data))
+(defun random-nth (coll)
+  (nth coll
+       (sample (uniform-discrete 0 (count coll)))))
 
-  [assume iris-class (lambda (x)
-                       (if (sample (flip (z x)))
-                         iris-setosa
-                         iris-virginica))]
+(with-primitive-procedures [dot]
+  (defanglican lr-iris
+    [assume features (lambda (record) (cons 1 (butlast record)))]
+    ;; choose random samples from the dataset, one from each class
+    [assume test-setosa (features (random-nth iris-data-setosa))]
+    [assume test-not-setosa (features (random-nth iris-data-not-setosa))]
+    ;; removing test data from the training set
+    [assume iris-data (filter (lambda (record)
+                                (not (or (= record test-setosa)
+                                         (= record test-not-setosa))))
+                              iris-data)]
 
-  [assume test-setosa '( 1 5.1 3.5 1.4 0.2)]
-  [predict (iris-class test-setosa)]
-  [assume test-virginica '(1 7.7 2.6 6.9 2.3)]
-  [predict (iris-class test-virginica)])
+    [assume sigma (sqrt (sample (gamma 1 1)))]
+    [assume b (repeatedly
+                5 (lambda () (sample (normal 0. sigma))))]
+    [assume z (lambda (x) 
+                (/ 1. (+ 1. (exp (* -1. (dot b x))))))]
+
+    (reduce (lambda (_ record)
+              (observe (flip (z (features record)))
+                       (= (last record) iris-setosa)))
+            () iris-data)
+
+    [assume is-setosa (lambda (x) (sample (flip (z x))))]
+    [predict (is-setosa test-setosa)]
+    [predict (is-setosa test-not-setosa)]))
