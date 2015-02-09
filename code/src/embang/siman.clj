1f0f4eb49d2213a04de80d4ae046fe8eb2fe4abc
David Tolpin <david.tolpin@gmail.com>
2015-02-09 18:45:42 +0000

    better docstrings

--- code/src/embang/siman.clj
+++ code/src/embang/siman.clj
@@ -33,7 +33,7 @@
 (defrecord entry [choice-id value cont])
 
 (defn choice-id
-  "returns unique idenditifer for sample checkpoint
+  "returns an unique idenditifer for sample checkpoint
   and the updated state"
   [obs state]
   (checkpoint-id obs state ::choice-counts ::choice-last-id))
