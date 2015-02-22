91f95f961a9ca267c840c6f1097d09a195d91758
David Tolpin <david.tolpin@gmail.com>
2015-02-22 00:53:59 +0000

    added ctp examples

--- code/src/angsrc/scratch.clj
+++ code/src/angsrc/scratch.clj
@@ -3,9 +3,14 @@
 
 ;;; Scratch pad
 
-(defanglican scratch
-  [assume fact (lambda (n)
-                  (loop ((n n) (acc 1))
-                    (if (<= n 1) acc
-                        (recur (- n 1) (* acc n)))))]
-  [predict (map fact (range 10))])
+(def-cps-fn foo []
+  (if 1 2 3
+  (predict (/ 1 0))))
+
+(defquery scratch
+  (let [x 1
+        y 2]
+    (predict x)
+    (predict y)
+    (predict 'sum (+ x y))
+    (foo)))
