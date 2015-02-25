6b458de2fb659a0b3b1ff4bd911774a0ac20dc68
David Tolpin <david.tolpin@gmail.com>
2015-02-25 00:12:09 +0000

    a different parallelization for lmh

--- code/src/embang/plmh.clj
+++ code/src/embang/plmh.clj
@@ -13,12 +13,14 @@
   returning a lazy sequence of results"
   [thunk n]
   (letfn [(result-seq [running]
-            (lazy-seq 
-              (cons (deref (peek running))
-                    (result-seq (conj (pop running)
-                                      (future (thunk)))))))]
-    (result-seq (into clojure.lang.PersistentQueue/EMPTY
-                      (repeatedly n #(future (thunk)))))))
+            (if (empty? running)
+              (recur
+                (into running
+                      (repeatedly n #(future (thunk)))))
+              (lazy-seq
+                (cons (deref (peek running))
+                      (result-seq (pop running))))))]
+    (result-seq clojure.lang.PersistentQueue/EMPTY)))
 
 (defn next-state-seq
   "returns lazy sequence of next states"
