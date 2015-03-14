03547b9cb626f10466be6df921e4d3bd30af5e8b
David Tolpin <david.tolpin@gmail.com>
2015-03-14 21:45:04 +0000

    version up

--- code/project.clj
+++ code/project.clj
@@ -1,4 +1,4 @@
-(defproject embang "0.4.11-SNAPSHOT"
+(defproject embang "0.4.12-SNAPSHOT"
   :description "Anglican, a probabilistic programming system"
   :url "http://bitbucket.org/dtolpin/embang"
   :license {:name "GNU General Public License Version 3"
