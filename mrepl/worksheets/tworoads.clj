f9d18b11524b04ae3b0f17cccfa832b423da51b4
David Tolpin <david.tolpin@gmail.com>
2015-03-16 00:40:33 +0000

    a much, much improved example of two roads

--- mrepl/worksheets/tworoads.clj
+++ mrepl/worksheets/tworoads.clj
@@ -28,19 +28,19 @@
 (def cb 2)
 (def p1 1/3)
 (def p2 2/3)
-(def gas-cost 100)
 
-(defquery tworoads "two roads example"
-  (let [o1 (sample (flip p1))
+(defquery tworoads "two roads example" gas-cost
+  (let [gas-cost (or gas-cost 1.)
+        o1 (sample (flip p1))
         o2 (sample (flip p2))
-        q (sample (uniform-continuous 0. 1.))
-        s (sample (flip q))]
+        q (sample (beta (/ 1. gas-cost) (/ 1. gas-cost)))]
     (observe (flip 1.0) (or o1 o2))
 
     (let [distance (if (sample (flip q))
                      (if o1 c1 (+ c2 cb))
-                     (if o2 c2 (+ c1 cb)))]
-      (observe (flip (exp (- (* gas-cost distance)))) true)
+                     (if o2 c2 (+ c1 cb)))
+          log-weight (* distance gas-cost)]
+      (observe (flip (exp (- log-weight))) true)
       (predict :q q))))
 ;; @@
 ;; =>
@@ -48,11 +48,11 @@
 ;; <=
 
 ;; **
-;;; Now we want to run (lazily some inference). We'll use LMH (Lightweight Metropolis-Hastings) for that.
+;;; Now we want to run (lazily) some inference. We'll use LMH (Lightweight Metropolis-Hastings) for that.
 ;; **
 
 ;; @@
-(def states (doquery :lmh tworoads nil))
+(def states (doquery :almh tworoads 1.))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tworoads/states</span>","value":"#'tworoads/states"}
@@ -63,7 +63,7 @@
 ;; **
 
 ;; @@
-(def N 10000)
+(def N 2000)
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tworoads/N</span>","value":"#'tworoads/N"}
@@ -80,7 +80,18 @@
 
 ;; @@
 ;; =>
-;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"c32d26c9-5752-4761-a36a-094601bff497","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"c32d26c9-5752-4761-a36a-094601bff497","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"c32d26c9-5752-4761-a36a-094601bff497"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"c32d26c9-5752-4761-a36a-094601bff497","values":[{"x":2.7038343250751495E-4,"y":0},{"x":0.06637863665819169,"y":1012.0},{"x":0.13248688988387586,"y":1059.0},{"x":0.19859514310956003,"y":1219.0},{"x":0.2647033963352442,"y":1140.0},{"x":0.33081164956092834,"y":765.0},{"x":0.3969199027866125,"y":954.0},{"x":0.46302815601229663,"y":809.0},{"x":0.5291364092379808,"y":610.0},{"x":0.5952446624636649,"y":606.0},{"x":0.6613529156893491,"y":616.0},{"x":0.7274611689150332,"y":408.0},{"x":0.7935694221407174,"y":320.0},{"x":0.8596776753664015,"y":240.0},{"x":0.9257859285920856,"y":159.0},{"x":0.9918941818177698,"y":77.0},{"x":1.058002435043454,"y":6.0},{"x":1.1241106882691383,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"c32d26c9-5752-4761-a36a-094601bff497\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"c32d26c9-5752-4761-a36a-094601bff497\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"c32d26c9-5752-4761-a36a-094601bff497\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"c32d26c9-5752-4761-a36a-094601bff497\", :values ({:x 2.7038343250751495E-4, :y 0} {:x 0.06637863665819169, :y 1012.0} {:x 0.13248688988387586, :y 1059.0} {:x 0.19859514310956003, :y 1219.0} {:x 0.2647033963352442, :y 1140.0} {:x 0.33081164956092834, :y 765.0} {:x 0.3969199027866125, :y 954.0} {:x 0.46302815601229663, :y 809.0} {:x 0.5291364092379808, :y 610.0} {:x 0.5952446624636649, :y 606.0} {:x 0.6613529156893491, :y 616.0} {:x 0.7274611689150332, :y 408.0} {:x 0.7935694221407174, :y 320.0} {:x 0.8596776753664015, :y 240.0} {:x 0.9257859285920856, :y 159.0} {:x 0.9918941818177698, :y 77.0} {:x 1.058002435043454, :y 6.0} {:x 1.1241106882691383, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"1149080c-1772-4b9d-abcc-9e988b1f21a2","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"1149080c-1772-4b9d-abcc-9e988b1f21a2","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"1149080c-1772-4b9d-abcc-9e988b1f21a2"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"1149080c-1772-4b9d-abcc-9e988b1f21a2","values":[{"x":0.0014272206462919712,"y":0},{"x":0.08451909463231763,"y":304.0},{"x":0.1676109686183433,"y":263.0},{"x":0.2507028426043689,"y":235.0},{"x":0.3337947165903946,"y":242.0},{"x":0.4168865905764203,"y":192.0},{"x":0.499978464562446,"y":203.0},{"x":0.5830703385484717,"y":156.0},{"x":0.6661622125344974,"y":126.0},{"x":0.749254086520523,"y":74.0},{"x":0.8323459605065487,"y":101.0},{"x":0.9154378344925744,"y":59.0},{"x":0.9985297084786001,"y":45.0},{"x":1.0816215824646258,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"1149080c-1772-4b9d-abcc-9e988b1f21a2\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"1149080c-1772-4b9d-abcc-9e988b1f21a2\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"1149080c-1772-4b9d-abcc-9e988b1f21a2\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"1149080c-1772-4b9d-abcc-9e988b1f21a2\", :values ({:x 0.0014272206462919712, :y 0} {:x 0.08451909463231763, :y 304.0} {:x 0.1676109686183433, :y 263.0} {:x 0.2507028426043689, :y 235.0} {:x 0.3337947165903946, :y 242.0} {:x 0.4168865905764203, :y 192.0} {:x 0.499978464562446, :y 203.0} {:x 0.5830703385484717, :y 156.0} {:x 0.6661622125344974, :y 126.0} {:x 0.749254086520523, :y 74.0} {:x 0.8323459605065487, :y 101.0} {:x 0.9154378344925744, :y 59.0} {:x 0.9985297084786001, :y 45.0} {:x 1.0816215824646258, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;; <=
+
+;; **
+;;; Estimating the mode numerically can be problematic, but we can compute the 'mean' policy easily.
+;; **
+
+;; @@
+(/ (reduce + policy) (count policy))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-double'>0.3596851939330809</span>","value":"0.3596851939330809"}
 ;; <=
 
 ;; **
@@ -89,5 +100,99 @@
 ;;; @@q = \arg \max\limits_q \left[q\color{blue}{(p_1c_1+(1-p_1)(c_b+c_2))} + (1-q)\color{brown}{(p_2c_2+(1-p_2)(c_b+c_1))}\right]@@
 ;;; 
 ;;; If @@\color{blue} {(p_1c_1+(1-p_1)(c_b+c_2))}@@ is less than @@\color{brown} {(p_2c_2+(1-p_2)(c_b+c_1)}@@,
-;;; q must be 1, that is, the first road should always be checked first. Otherwise, q must  be 0.
+;;; q must be 1, that is, the first road should always be checked first. Otherwise, q must  be 0.k
+;; **
+
+;; **
+;;; ## Simulated Annealing using non-uniform priors
+;;; 
+;;; We would like to modify the inference so that we can numerically, rather than visually, discover the optimal policy. The SAME algorithm could be an option if we had an easy way to implement it for probabilistic programming.
+;;; 
+;;; However, there is another way: instead of selecting the policy from the uniform prior, we can gradually 'tighten' the prior so that in the beginning the prior is uniform, but as we proceed, we give mixed policies an increasingly low chance. We do this in M steps.
+;; **
+
+;; @@
+(def M 15)
+
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tworoads/M</span>","value":"#'tworoads/M"}
+;; <=
+
+;; **
+;;; Growing gas-cost linearly is most probably too slow, we will grow it exponentially.
+;; **
+
+;; @@
+(let  [c 1.5]
+  (def gas-costs (map #(pow c %) (range M)))
+  (map round gas-costs))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"},{"type":"html","content":"<span class='clj-long'>5</span>","value":"5"},{"type":"html","content":"<span class='clj-long'>8</span>","value":"8"},{"type":"html","content":"<span class='clj-long'>11</span>","value":"11"},{"type":"html","content":"<span class='clj-long'>17</span>","value":"17"},{"type":"html","content":"<span class='clj-long'>26</span>","value":"26"},{"type":"html","content":"<span class='clj-long'>38</span>","value":"38"},{"type":"html","content":"<span class='clj-long'>58</span>","value":"58"},{"type":"html","content":"<span class='clj-long'>86</span>","value":"86"},{"type":"html","content":"<span class='clj-long'>130</span>","value":"130"},{"type":"html","content":"<span class='clj-long'>195</span>","value":"195"},{"type":"html","content":"<span class='clj-long'>292</span>","value":"292"}],"value":"(1 2 2 3 5 8 11 17 26 38 58 86 130 195 292)"}
+;; <=
+
+;; **
+;;; At every step, we run the inference with a higher value of gas cost. As the gas cost goes up, both the prior and the log weight cause the distribution to become more 'peaked'. For each step, we compute the 'mean' policy.
+;; **
+
+;; @@
+(def mean-policy (for [gas-cost gas-costs]
+              	   (let [states (doquery :almh tworoads gas-cost :number-of-particles 100)
+                   		 predicts (map get-predicts (take N states))
+                         policy (map :q predicts)]
+                     (/ (reduce + policy) (count policy)))))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tworoads/mean-policy</span>","value":"#'tworoads/mean-policy"}
+;; <=
+
+;; **
+;;; We can now plot the mean policies to see where they converge to:
+;; **
+
+;; @@
+(plot/list-plot mean-policy :joined true)
+;; @@
+;; =>
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"5a7051cb-97da-419c-a72a-2e95be85bc9f","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"5a7051cb-97da-419c-a72a-2e95be85bc9f","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"5a7051cb-97da-419c-a72a-2e95be85bc9f"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#FF29D2"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"5a7051cb-97da-419c-a72a-2e95be85bc9f","values":[{"x":0,"y":0.37181330660381356},{"x":1,"y":0.29275314951407005},{"x":2,"y":0.25470776627539543},{"x":3,"y":0.178810977880195},{"x":4,"y":0.14461463445136724},{"x":5,"y":0.11865879514327274},{"x":6,"y":0.19379728906821697},{"x":7,"y":0.04406318602055257},{"x":8,"y":0.03849107724846605},{"x":9,"y":0.017474489016950832},{"x":10,"y":0.011248471259466545},{"x":11,"y":0.020711144609878156},{"x":12,"y":0.4803097857609864},{"x":13,"y":0.23512149589788303},{"x":14,"y":0.0023094604280568926}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"5a7051cb-97da-419c-a72a-2e95be85bc9f\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"5a7051cb-97da-419c-a72a-2e95be85bc9f\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"5a7051cb-97da-419c-a72a-2e95be85bc9f\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#FF29D2\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"5a7051cb-97da-419c-a72a-2e95be85bc9f\", :values ({:x 0, :y 0.37181330660381356} {:x 1, :y 0.29275314951407005} {:x 2, :y 0.25470776627539543} {:x 3, :y 0.178810977880195} {:x 4, :y 0.14461463445136724} {:x 5, :y 0.11865879514327274} {:x 6, :y 0.19379728906821697} {:x 7, :y 0.04406318602055257} {:x 8, :y 0.03849107724846605} {:x 9, :y 0.017474489016950832} {:x 10, :y 0.011248471259466545} {:x 11, :y 0.020711144609878156} {:x 12, :y 0.4803097857609864} {:x 13, :y 0.23512149589788303} {:x 14, :y 0.0023094604280568926})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;; <=
+
+;; **
+;;; We observe two things: 
+;;; * the 'mean' policy indeed converges to the best policy
+;;; * however, as the gas cost goes up, the result becomes noiser.
+;;; 
+;;; The former is good, but the latter has to be fixed. We can fix it by increasing the number of iterations with the gas cost:
+;; **
+
+;; @@
+(def max-cost (last gas-costs))
+(def mean-policy (for [gas-cost gas-costs]
+              	   (let [states (doquery :lmh tworoads gas-cost :number-of-particles 100)
+                   		 predicts (map get-predicts
+                                       (take (int (* (+ 1. (* 9. (/ gas-cost max-cost))) N))
+                                             states))
+                         policy (map :q predicts)]
+                     (/ (reduce + policy) (count policy)))))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tworoads/mean-policy</span>","value":"#'tworoads/mean-policy"}
+;; <=
+
+;; **
+;;; We expect to see the same trend, but with less noise for higher values of gas cost.
+;; **
+
+;; @@
+(plot/list-plot mean-policy :joined true)
+;; @@
+;; =>
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"69fa67a9-24a1-4d06-86ce-5888fda9dcf0","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"69fa67a9-24a1-4d06-86ce-5888fda9dcf0","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"69fa67a9-24a1-4d06-86ce-5888fda9dcf0"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#FF29D2"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"69fa67a9-24a1-4d06-86ce-5888fda9dcf0","values":[{"x":0,"y":0.3675874328447035},{"x":1,"y":0.3155354914335795},{"x":2,"y":0.2486533366058317},{"x":3,"y":0.18895454812478926},{"x":4,"y":0.16108322010164086},{"x":5,"y":0.09833949825600763},{"x":6,"y":0.0768461785255041},{"x":7,"y":0.06259059918952636},{"x":8,"y":0.03240327503590268},{"x":9,"y":0.031009962088013678},{"x":10,"y":0.01338857663221404},{"x":11,"y":0.014402817188910219},{"x":12,"y":0.009192124254480202},{"x":13,"y":0.02073259696015582},{"x":14,"y":0.011851453396037846}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"69fa67a9-24a1-4d06-86ce-5888fda9dcf0\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"69fa67a9-24a1-4d06-86ce-5888fda9dcf0\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"69fa67a9-24a1-4d06-86ce-5888fda9dcf0\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#FF29D2\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"69fa67a9-24a1-4d06-86ce-5888fda9dcf0\", :values ({:x 0, :y 0.3675874328447035} {:x 1, :y 0.3155354914335795} {:x 2, :y 0.2486533366058317} {:x 3, :y 0.18895454812478926} {:x 4, :y 0.16108322010164086} {:x 5, :y 0.09833949825600763} {:x 6, :y 0.0768461785255041} {:x 7, :y 0.06259059918952636} {:x 8, :y 0.03240327503590268} {:x 9, :y 0.031009962088013678} {:x 10, :y 0.01338857663221404} {:x 11, :y 0.014402817188910219} {:x 12, :y 0.009192124254480202} {:x 13, :y 0.02073259696015582} {:x 14, :y 0.011851453396037846})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;; <=
+
+;; **
+;;; Much better now. Of course, the 'cooling schedule', both the change in the gas cost, and the  increase of the number of iterations, can be smarter. 
+;;; 
+;;; Also, we do not have to start the inference from scratch for each increase of gas-cost. Instead, we can continue the same inference in a way similar to Simulated Annealing.
 ;; **
