51c5119388c29bace3fea9b4f3d259bbe62a54eb
David Tolpin <david.tolpin@gmail.com>
2015-03-18 15:25:05 +0000

    warmup is an option

--- mrepl/src/mrepl/core.clj
+++ mrepl/src/mrepl/core.clj
@@ -9,14 +9,22 @@
 (defn doquery
   "performs inference query;
   returns lazy sequence of states"
-  [algorithm program value & options]
+  [algorithm program value & {:keys [warmup]
+                              :or {warmup true}
+                              :as options}]
   (do
     ;; Use the auto-loading machine in embang.core to load
     ;; the inference algorithm on demand.
     (load-algorithm algorithm)
-    ;; Then, call the inference to create a lazy sequence
-    ;; of states.
-    (apply infer algorithm (warmup program value) nil options)))
+    ;; Optionally, warm up the program by pre-evaluating
+    ;; the determenistic prefix.
+    (let [[program value] (if warmup 
+                            [(warmup program value) nil]
+                            [program value])]
+      ;; Finally, call the inference to create
+      ;; a lazy sequence of states.
+      (apply infer algorithm program value
+             (apply concat options)))))
 
 ;; State readers
 
