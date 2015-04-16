4a4146daed37e3701060acd569d4dc8fc5cc1c48
David Tolpin <david.tolpin@gmail.com>
2015-04-16 21:37:42 +0100

    moved

--- code/src/embang/rdb.clj
+++
@@ -1,8 +0,0 @@
-(ns anglican.rdb
-  "Random DB, same as Lightweight Metropolis-Hastings"
-  (:refer-clojure :exclude [rand rand-int rand-nth])
-  (:use anglican.inference))
-
-;;;; Alias for Single-site Metropolis-Hastings
-
-(defmethod infer :rdb [algorithm & args] (apply infer :lmh args))
--- code/test/embang/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns anglican.core-test
-  (:require [clojure.test :refer :all]
-            [anglican.core :refer :all]))
