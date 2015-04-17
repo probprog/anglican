56ec92dbf2cf4506b437e7054dc6ed4d7bb5b073
David Tolpin <david.tolpin@gmail.com>
2015-04-17 14:21:50 +0100

    version up

--- code/project.clj
+++ code/project.clj
@@ -1,4 +1,4 @@
-(defproject anglican "0.6.5-SNAPSHOT"
+(defproject anglican "0.6.6-SNAPSHOT"
   :description "Anglican, a probabilistic programming system"
   :url "http://bitbucket.org/dtolpin/anglican"
   :license {:name "GNU General Public License Version 3"
