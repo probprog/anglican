bb50fe35477982c4adcf683c656fb8a4275b504a
David Tolpin <david.tolpin@gmail.com>
2016-02-02 23:02:52 +0200

    libraries are brought up-to-date

--- code/project.clj
+++ code/project.clj
@@ -4,13 +4,13 @@
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/data.json "0.2.6"]
                  [com.taoensso/timbre "4.2.1"]
                  [colt "1.2.0"]
-                 [net.mikera/core.matrix "0.36.1"]
-                 [net.mikera/core.matrix.stats "0.6.0"]
-                 [net.mikera/vectorz-clj "0.30.1"]]
+                 [net.mikera/core.matrix "0.49.0"]
+                 [net.mikera/core.matrix.stats "0.7.0"]
+                 [net.mikera/vectorz-clj "0.43.0"]]
   :plugins [[codox "0.8.11"]]
   :scm {:name "git"
         :url "https://bitbucket.org/dtolpin/anglican"}
