3d5862f0d343dcb6e43e1b84040c5d2e3f1e0b14
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-02-21 09:19:23 -0500

    updated CRP tests again (third time lucky)

--- code/test/anglican/runtime_test.clj
+++ code/test/anglican/runtime_test.clj
@@ -36,15 +36,22 @@
 
 (deftest test-CRP
   (testing  "CRP"
-    (let [proc (CRP 1.0)]
-      (is (= (observe (produce (absorb proc 3)) 3) (Math/log 1/2))
-          "observing absorbed value")
-      (is (= (observe (produce (absorb proc 0)) 1) (Math/log 1/2))
-          "observing unabsorbed value greater than count")
-      (is (= (observe (produce (absorb proc 1)) 0) (Math/log 1/2))
-          "observing unabsorbed value less than count")
-      (is (= (observe (produce proc) 2) (Math/log 1))
-          "observing any new value"))))
+    (let [proc (CRP 0.1 {1 1 2 2 3 3})]
+      (is (= (observe (produce proc) 3) 
+             (Math/log (/ 3 6.1)))
+          "observing existing value")
+      (is (= (observe (produce proc) 0) 
+             (Math/log (/ 0.1 6.1)))
+          "observing new value")
+      (is (= (observe (produce (absorb proc 3)) 3) 
+             (Math/log (/ 4 7.1)))
+          "observing absorbed existing value")
+      (is (= (observe (produce (absorb proc 0)) 1) 
+             (Math/log (/ 1 7.1)))
+          "observing absorbed new value greater than count")
+      (is (= (observe (produce (absorb proc 1)) 0) 
+             (Math/log (/ 0.1 7.1)))
+          "observing unabsorbed value less than count"))))
 
 (deftest test-cov
   (testing "cov"
