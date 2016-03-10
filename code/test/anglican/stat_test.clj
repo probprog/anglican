b53ace11857f605f38a41b7e58001ff406e5e8c9
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-03-10 16:26:16 +0000

    added docstrings for unit tests of anglican.stat

--- code/test/anglican/stat_test.clj
+++ code/test/anglican/stat_test.clj
@@ -16,64 +16,84 @@
 
 (deftest test-sum
   (is (m/equals [12 15 18 21]
-                (sum test-array 0)))
+                (sum test-array 0))
+      "sum along dimension 0 returns correct result on test array")
   (is (m/equals [6 22 38]
-                (sum test-array 1)))
+                (sum test-array 1))
+      "sum along dimension 1 returns correct result on test array")
   (is (m/equals (sum test-array)
-                (sum test-array 0))))
+                (sum test-array 0))
+      "default sum is along dimension 0"))
 
 (deftest test-mean
   (is (m/equals [12/3 15/3 18/3 21/3]
-                (mean test-array 0)))
+                (mean test-array 0))
+      "mean along dimension 0 returns correct result on test array")
   (is (m/equals [6/4 22/4 38/4]
-                (mean test-array 1)))
+                (mean test-array 1))
+      "mean along dimension 1 returns correct result on test array")
   (is (m/equals (mean test-array)
-                (mean test-array 0))))
+                (mean test-array 0))
+      "default mean is along dimension 0"))
 
 (deftest test-variance
   (is (m/equals [32/3 32/3 32/3 32/3]
-                (variance test-array 0)))
+                (variance test-array 0))
+      "variance along dimension 0 returns correct result on test array")
   (is (m/equals [5/4 5/4 5/4]
-                (variance test-array 1)))
+                (variance test-array 1))
+      "variance along dimension 1 returns correct result on test array")
   (is (m/equals (variance test-array)
-                (variance test-array 0))))
+                (variance test-array 0))
+      "default is along dimension 0"))
 
 (deftest test-covariance
   (is (m/equals [[32/3 32/3 32/3 32/3]
                  [32/3 32/3 32/3 32/3]
                  [32/3 32/3 32/3 32/3]
                  [32/3 32/3 32/3 32/3]]
-                (covariance test-array 0)))
+                (covariance test-array 0))
+      "covariance along dimension 0 returns correct result on test array")
   (is (m/equals [[5/4 5/4 5/4]
                  [5/4 5/4 5/4]
                  [5/4 5/4 5/4]]
-                (covariance test-array 1)))
+                (covariance test-array 1))
+      "covariance along dimension 1 returns correct result on test array")
   (is (m/equals (covariance test-array)
-                (covariance test-array 0))))
+                (covariance test-array 0))
+      "default covariance is along dimension 0"))
 
 (deftest test-std
   (is (m/equals (m/sqrt [32/3 32/3 32/3 32/3])
-                (std test-array 0)))
+                (std test-array 0))
+      "std along dimension 0 returns correct result on test array")
   (is (m/equals (m/sqrt [5/4 5/4 5/4])
-                (std test-array 1)))
+                (std test-array 1))
+      "std along dimension 0 returns correct result on test array")
   (is (m/equals (std test-array)
-                (std test-array 0))))
+                (std test-array 0))
+      "default std is along dimension 0"))
 
 (deftest test-l2
   (is (m/equals 48
-                (l2-norm test-array (m/add test-array 2)))))
+                (l2-norm test-array (m/add test-array 2)))
+      "l2 returns correct result when applied to array and its shifted form"))
 
 (deftest test-empirical-expectation
   (is (m/equals 5.0
                 (empirical-expectation identity test-weighted)
-                tolerance)))
+                tolerance)
+      "empirical-expectation returns correct mean for test sample set from normal"))
 
 (deftest test-empirical-mean
   (is (m/equals 5.0
                 (empirical-mean test-weighted)
-                tolerance)))
+                tolerance)
+      "empirical-mean returns correct mean for test sample set from normal"))
 
 (deftest test-empirical-distribution
   (is (m/equals 1.0
                 (reduce + (vals (empirical-distribution test-weighted)))
-                tolerance)))
+                tolerance)
+      "empirical-distribution returns weights that are normalized to 1.0"))
+  
