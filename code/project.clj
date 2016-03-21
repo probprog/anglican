43740efd32988cc967d17aa263eacd848f56b53b
David Tolpin <david.tolpin@gmail.com>
2016-03-22 01:06:02 +0200

    up-to-date versions of dependencies

--- code/project.clj
+++ code/project.clj
@@ -1,14 +1,14 @@
-(defproject probprog/anglican "0.9.0-SNAPSHOT"
+(defproject probprog/anglican "0.10.0-SNAPSHOT"
   :description "Anglican, a probabilistic programming system"
   :url "http://bitbucket.org/dtolpin/anglican"
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.taoensso/timbre "4.3.1"]
                  [colt "1.2.0"]
-                 [net.mikera/core.matrix "0.49.0"]
+                 [net.mikera/core.matrix "0.50.0"]
                  [net.mikera/core.matrix.stats "0.7.0"]
                  [net.mikera/vectorz-clj "0.43.1"]]
   :plugins [[codox "0.8.11"]]
