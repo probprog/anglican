ec5fa0c9ba19e43d918eb2b46f0ce98fb806470e
David Tolpin <david.tolpin@gmail.com>
2016-03-25 04:10:34 +0300

    synchronized MersenneTwister

--- code/src/anglican/MersenneTwister.java
+++ code/src/anglican/MersenneTwister.java
@@ -6,23 +6,6 @@ package anglican;
 */
 public class MersenneTwister 
 	extends cern.jet.random.engine.MersenneTwister {
-
-	/* mimicking the base class */
-	private int mti = 0;
-	private static final int N = 624;
-
 	public MersenneTwister(java.util.Date d) {super(d);}
-
-	public int nextInt() {
-		if(mti >= N) {
-			synchronized(this) {
-				nextBlock();
-				mti = 0;
-			}
-		}
-		synchronized(this) {
-			mti++;
-		}
-		return super.nextInt();
-	}
+	public synchronized int nextInt() {return super.nextInt();}
 }
