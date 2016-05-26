4dce4289f415153eefb317d8da2a8e98786a0da6
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-05-26 19:02:41 +0100

    updated dependencies in project.clj

--- code/project.clj
+++ code/project.clj
@@ -4,14 +4,14 @@
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.json "0.2.6"]
                  [com.climate/claypoole "1.1.2"]
                  [com.taoensso/timbre "4.3.1"]
-                 [org.apache.commons/commons-math3 "3.6"]
-                 [net.mikera/core.matrix "0.50.0"]
+                 [org.apache.commons/commons-math3 "3.6.1"]
+                 [net.mikera/core.matrix "0.52.0"]
                  [net.mikera/core.matrix.stats "0.7.0"]
-                 [net.mikera/vectorz-clj "0.43.1"]]
+                 [net.mikera/vectorz-clj "0.45.0"]]
   :plugins [[codox "0.8.11"]]
   :scm {:name "git"
         :url "https://bitbucket.org/dtolpin/anglican"}
