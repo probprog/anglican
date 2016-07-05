4421e6712689df71abcc5040162b3cc0db17d32d
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-05 13:57:11 +0100

    inference: include :value in stripdown command

--- code/src/anglican/inference.clj
+++ code/src/anglican/inference.clj
@@ -103,7 +103,7 @@
 (defn stripdown
   "removes algorithm-specific entries from the state"
   [state]
-  (select-keys state [:log-weight :predicts]))
+  (select-keys state [:log-weight :value :predicts]))
 
 ;;; Random functions for inference algorithms
 
