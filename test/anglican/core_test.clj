9524ed1bb06eaf9dbf2b37cf9b17a917ca634e5d
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-20 13:08:42 +0100

    added test for new stripdown behavior

--- test/anglican/core_test.clj
+++ test/anglican/core_test.clj
@@ -1,3 +1,14 @@
 (ns anglican.core-test
-  (:require [clojure.test :refer :all]
-            [anglican.core :refer :all]))
+  (:require [clojure.test :refer :all])
+  (use [anglican core emit runtime]))
+
+(deftest stripdown-test
+  (let [q (query []
+            (let [theta (sample (normal 0 1))]
+              (observe (gamma 1 1) theta)
+              theta))]
+    (is (every? (fn [s]
+                  (> (:log-weight s) (/ -1.0 0.0)))
+                (take 100 (doquery :importance q [])))
+        (str "doquery should not return zero-weight samples when"
+             ":stripdown option is set"))))
