cee3d51678b59fad28e45142d9e911262de70ced
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-21 16:22:47 +0100

    added :drop-invalid option to doquery to filter out zero-weight samples

--- test/anglican/core_test.clj
+++ test/anglican/core_test.clj
@@ -4,11 +4,23 @@
 
 (deftest stripdown-test
   (let [q (query []
-            (let [theta (sample (normal 0 1))]
-              (observe (gamma 1 1) theta)
-              theta))]
+                 (let [theta (sample (normal 0 1))]
+                   (observe (gamma 1 1) theta)
+                   theta))]
+    (is (every? (fn [s]
+                  (> (:log-weight s) (/ -1.0 0.0)))
+                (take 100 (doquery :importance q [] 
+                                   :drop-invalid true)))
+        (str "doquery should not return zero-weight samples when"
+             ":drop-invalid option is set to true"))
+    (is (some (fn [s]
+                (= (:log-weight s) (/ -1.0 0.0)))
+              (take 100 (doquery :importance q [] 
+                                 :drop-invalid false)))
+        (str "doquery should return zero-weight samples when"
+             ":drop-invalid option is set to false"))
     (is (every? (fn [s]
                   (> (:log-weight s) (/ -1.0 0.0)))
                 (take 100 (doquery :importance q [])))
         (str "doquery should not return zero-weight samples when"
-             ":stripdown option is set"))))
+             ":drop-invalid option is not specified"))))
